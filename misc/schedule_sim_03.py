d92fb3a1c82e642a1364deba8cac4afb4ecfcf50
ingajsa <inga.sauer@pik-potsdam.de>
2021-05-01 02:33:58 +0200

    add netcdf conversion

--- misc/schedule_sim_03.py
+++ misc/schedule_sim_03.py
@@ -91,3 +91,11 @@ for n,iso in enumerate(iso3_list[1:]):
     gdpa = gdpa.append(gdpa1)
 
 gdpa.write_hdf5('/home/insauer/data/plot_exposure/global_exposure_2000.h5')
+raster, meta = u_coord.points_to_raster(gdpa, ['value'], res=None, raster_res=0.25, scheduler='threads')
+tifffile = "/home/insauer/data/plot_exposure/global_exposure_2000.tiff"
+u_coord.write_raster(tifffile, raster, meta)
+
+netcdffile = "/home/insauer/data/plot_exposure/global_exposure_2000.nc"
+
+#Do not change this line, the following command will convert the geoTIFF to a netCDF
+gdal.Translate(netcdffile, tifffile, format='NetCDF')
